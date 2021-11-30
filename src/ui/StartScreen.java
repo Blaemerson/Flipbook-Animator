03ffac210dad4eb4972fbd0459de2aba7ddde6e9
Wyent <dwyent@gmail.com>
2021-11-30 14:42:00 -0600

    skeleton of startScreen

---
+++ src/ui/StartScreen.java
@@ -0,0 +1,25 @@
+package ui;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.application.Application;
+
+import java.io.IOException;
+
+public class StartScreen extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(StartScreen.class.getResource("resources/window-start.fxml"));
+
+        Scene scene = new Scene(fxmlLoader.load(), 480, 360);
+
+        stage.setTitle("Onionskin Studio");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
